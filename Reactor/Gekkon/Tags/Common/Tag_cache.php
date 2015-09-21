5f21a7cc6c277367c8b09fa0e3530918403004ca
snow-ghost <specm32@gmail.com>
2015-09-21 10:34:34 +0500

    refactor fields of gekkon & add get_module to module manager

--- Reactor/Gekkon/Tags/Common/Tag_cache.php
+++ Reactor/Gekkon/Tags/Common/Tag_cache.php
@@ -19,8 +19,8 @@ class Tag_cache extends BaseTag {
         } //save it forever!
         $id = $args['id'];
         $timeout = $args['timeout'];
-        return "\$_gkn_cache=\$gekkon->cacheProvider->load(\$template,$id);\n" . "if(\$_gkn_cache===false || (time()-\$_gkn_cache['created']>$timeout && $timeout!==0)){\n" .
-               "ob_start();\n" . $compiler->compile_str($this->content_raw, $this) . "\$gekkon->cacheProvider->save(\$template,\$_gkn_cache_show=ob_get_clean(),$id);\n" .
+        return "\$_gkn_cache=\$gekkon->cache_provider->load(\$template,$id);\n" . "if(\$_gkn_cache===false || (time()-\$_gkn_cache['created']>$timeout && $timeout!==0)){\n" .
+               "ob_start();\n" . $compiler->compile_str($this->content_raw, $this) . "\$gekkon->cache_provider->save(\$template,\$_gkn_cache_show=ob_get_clean(),$id);\n" .
                "echo \$_gkn_cache_show;\n" . "}else{\n" . "echo \$_gkn_cache['content'];\n" . "}\n";
     }
 }
