25da75879d566f945a0d362d410297ead91bdf9a
snow-ghost <specm32@gmail.com>
2016-01-16 00:29:11 +0500

    merge with master

--- Reactor/Memcache/Module.php
+++ Reactor/Memcache/Module.php
@@ -3,9 +3,9 @@ namespace Reactor\Memcache;
 
 class Module extends \Reactor\Application\Module {
 
-    public function init($container, $config = array()) {
-        $confugurator = parent::init($container, $config);
-        foreach ($this->data as $key => $value) {
+    public function configure($container, $config = array()) {
+        $confugurator = parent::configure($container, $config);
+        foreach ($this->get('connections') as $key => $value) {
             $servers = array();
             foreach ($value as $item) {
                 $servers[] = array($item['host'], $item['port']);
--- htdocs/index.php
+++ htdocs/index.php
@@ -3,6 +3,8 @@
 $start = microtime(true);
 $app = include '../bootstrap.php';
 echo (microtime(true) - $start) ."\n";
+$app->cache->set('aaa', 60);
+echo $app->cache->get('aaa');
 //$m = $app;
 //$m->__sleep();
 //var_export($m);
